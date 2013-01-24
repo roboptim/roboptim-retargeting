cb2192c002541de8eefde3b341a51f21a61c02a9
Thomas Moulard <thomas.moulard@gmail.com>
2013-01-24 14:49:17 +0900

    Implement bone length computation.

--- src/bone-length.cc
+++ src/bone-length.cc
@@ -24,7 +24,34 @@ namespace roboptim
     (result_t& result, const argument_t& x)
       const throw ()
     {
-      result[1] = 0.;
+      InteractionMesh::vertex_descriptor_t source =
+	boost::source (edgeId_,
+		       animatedMesh_->meshes ()[frameId_]->graph ());
+      InteractionMesh::vertex_descriptor_t target =
+	boost::target (edgeId_,
+		       animatedMesh_->meshes ()[frameId_]->graph ());
+
+      const Vertex& sourceVertex =
+	animatedMesh_->meshes ()[frameId_]->graph ()[source];
+      const Vertex& targetVertex =
+	animatedMesh_->meshes ()[frameId_]->graph ()[target];
+      const Edge& edge =
+	animatedMesh_->meshes ()[frameId_]->graph ()[edgeId_];
+
+      long unsigned int nVertices =
+	boost::num_vertices (animatedMesh_->meshes ()[frameId_]->graph ());
+      InteractionMeshShPtr_t newMesh =
+	InteractionMesh::makeFromOptimizationVariables
+	(x.segment (frameId_ * nVertices * 3, nVertices * 3));
+      const Vertex& sourceVertexNew =
+	newMesh->graph ()[source];
+      const Vertex& targetVertexNew =
+	newMesh->graph ()[target];
+
+      result[1] =
+	(targetVertex.position - sourceVertex.position).squaredNorm ();
+      result[1] -= edge.scale *
+	(targetVertexNew.position - sourceVertexNew.position).squaredNorm ();
     }
 
     void
