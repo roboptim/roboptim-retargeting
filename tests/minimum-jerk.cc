50c22dd40eee6cd7443bd931557b7f75e9f8a5bb
Thomas Moulard <thomas.moulard@gmail.com>
2013-08-08 20:17:40 +0900

    Add empty minimum-jerk test

---
+++ tests/minimum-jerk.cc
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE acceleration_energy
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+
+#include "tests-config.h"
+
+using boost::test_tools::output_test_stream;
+
+BOOST_AUTO_TEST_CASE (simple)
+{
+  configureLog4cxx ();
+}
