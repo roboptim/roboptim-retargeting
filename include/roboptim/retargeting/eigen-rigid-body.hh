0a31060255d2b40cecbbb122c4516de6201d8c60
Thomas Moulard <thomas.moulard@gmail.com>
2014-02-19 16:14:51 +0900

    Add missing files

---
+++ include/roboptim/retargeting/eigen-rigid-body.hh
@@ -0,0 +1,161 @@
+#ifndef ROBOPTIM_RETARGETING_EIGEN_RIGID_BODY_HH
+# define ROBOPTIM_RETARGETING_EIGEN_RIGID_BODY_HH
+// FIXME: to include Eigen correctly.
+# include <roboptim/core/function.hh>
+
+/// \brief Set the rotational part of a transformation from
+///        Euler angles parameters.
+///
+/// \b Formula
+///
+/// \f$\alpha\f$ is the yaw parameter (rotation around Z axis)
+/// \f$\beta\f$ is the pitch parameter (rotation around Y axis)
+/// \f$\gamma\f$ is the roll parameter (rotation around X axis)
+///
+/// \f$ \left( \begin{array}{ccc}
+/// \cos(\alpha) \cos(\beta) &
+/// \cos(\alpha) \sin(\beta) \sin(\gamma) - \sin(\alpha) \cos(\gamma) &
+/// \cos(\alpha) \sin(\beta) \cos(\gamma) + \sin(\alpha) \sin(\gamma) \\
+/// \sin(\alpha) \cos(\beta) &
+/// \sin(\alpha) \sin(\beta) \sin(\gamma) + \cos(\alpha) \cos(\gamma) &
+/// \sin(\alpha) \sin(\beta) \cos(\gamma) - \cos(\alpha) \sin(\gamma) \\
+/// -\sin(\beta) &
+/// \cos(\beta) \sin(\gamma) &
+/// \cos(\beta) \cos(\gamma)
+/// \end{array} \right) \f$
+///
+/// \b Sources
+///
+/// http://planning.cs.uiuc.edu/node102.html
+///
+/// This implementation is an adaptation of the one in sot-core.
+///
+/// \param[out] eulerAngles vector \f$(\gamma, \beta, \alpha)\f$
+/// \param[in] transform store results
+template <typename Derived, typename OtherDerived>
+void
+eulerToTransform (Eigen::MatrixBase<Derived> const& transform,
+		  const Eigen::MatrixBase<OtherDerived>& eulerAngles);
+
+/// \brief Set the rotational part of a transformation from
+///        Euler angles parameters.
+///
+/// \b Notation
+///
+/// \f$\alpha\f$ is the yaw parameter (rotation around Z axis)
+/// \f$\beta\f$ is the pitch parameter (rotation around Y axis)
+/// \f$\gamma\f$ is the roll parameter (rotation around X axis)
+///
+/// \b Yaw
+/// \f$ \alpha = \tan^{-1}(r_{10} / r_{00}) \f$
+///
+/// \b Pitch
+/// \f$ \beta = \tan^{-1}(-r_{20} / \sqrt{r_{21}^2 + r_{22}^2}) \f$
+///
+/// \b Roll
+/// \f$ \gamma = \tan^{-1}(r_{21} / r_{22}) \f$
+///
+/// \b Sources
+///
+/// http://planning.cs.uiuc.edu/node103.html
+///
+/// This implementation is an adaptation of the one in sot-core.
+///
+/// \param[out] linear rotation matrix
+/// \param[in] eulerAngles vector \f$(\gamma, \beta, \alpha)\f$
+template <typename Derived, typename OtherDerived>
+void
+transformToEuler (Eigen::MatrixBase<Derived> const& eulerAngles,
+		  const Eigen::MatrixBase<OtherDerived>& linear);
+
+/// \brief Build a skew-symmetric matrix from vector of size 3.
+///
+/// \b Formula
+///
+/// \f$ \left( \begin{array}{ccc}
+/// 0 &
+/// -\mathbf{v}_2 &
+/// \mathbf{v}_1 \\
+/// \mathbf{v}_2 &
+/// 0 &
+/// -\mathbf{v}_0 \\
+/// -\mathbf{v}_1 &
+/// \mathbf{v}_0 &
+/// 0
+/// \end{array} \right) \f$
+///
+/// \param[out] result matrix (size 3x3)
+/// \param[in] vector \f$(\mathbf{v}_0, \mathbf{v}_1, \mathbf{v}_2)\f$
+template <typename Derived, typename OtherDerived>
+void hat (Eigen::MatrixBase<Derived> const& result,
+	  const Eigen::MatrixBase<OtherDerived>& p);
+
+/// \brief Build a skew-symmetric matrix from vector of size 3.
+///
+/// \see hat(eulerAngles, transform)
+/// \param[out] result matrix
+/// \param[in] vector \f$(\mathbf{v}_0, \mathbf{v}_1, \mathbf{v}_2)\f$
+template <typename Derived>
+Eigen::Matrix<typename Derived::RealScalar, 3, 3> hat
+(const Eigen::MatrixBase<Derived>& p);
+
+/// \brief Build a transformation adjoint.
+///
+/// It must be applied to a twist written as:
+/// \f$ \mathbf{\xi} = (\mathbf{v}, \mathbf{\omega}) \f$
+/// where \f$\mathbf{v}\f$ is the
+///
+/// \b Formula
+///
+/// \f$\mathbf{R}\f$ is the transformation rotation matrix (3x3).
+/// \f$\mathbf{p}\f$ is the transformation translation vector (size 3).
+///
+/// \f$ \left( \begin{array}{cc}
+/// \mathbf{R} & \hat{\mathbf{p}} \mathbf{R} \\
+/// \mathbf{0} & \mathbf{R}
+/// \end{array} \right) \f$
+///
+/// \param[out] adjoint adjoint matrix (size 6x6)
+/// \param[in]  transform transformation
+template <typename Derived, int _Dim, int _Mode, int _Options>
+void
+adjoint (Eigen::MatrixBase<Derived> const& adjoint,
+	 const Eigen::Transform<typename Derived::RealScalar,
+				_Dim, _Mode, _Options>& transform);
+
+/// \brief Store a transformation as a vector.
+///
+/// The output vector contains three parameters for translation and
+/// three for the rotation (Euler angles).
+///
+/// \f$v = (x, y, z, \gamma, \beta, \alpha)\f$
+///
+/// \see eulerToTransform transformToEuler
+/// \param[out] result result vector
+///                    \f$v = (x, y, z, \gamma, \beta, \alpha)\f$
+/// \param[in]  transform input transformation
+template <typename Derived, int _Dim, int _Mode, int _Options>
+void
+transformToVector (Eigen::MatrixBase<Derived> const& result,
+		   const Eigen::Transform<typename Derived::RealScalar,
+					  _Dim, _Mode, _Options>& transform);
+
+/// \brief Convert a vector to a transform.
+///
+/// The input vector must three parameters for translation and
+/// three for the rotation (Euler angles).
+///
+/// \f$v = (x, y, z, \gamma, \beta, \alpha)\f$
+///
+/// \see eulerToTransform transformToEuler
+/// \param[in]  transform output transformation
+/// \param[out] result parameters vector
+///             \f$v = (x, y, z, \gamma, \beta, \alpha)\f$
+template <typename Derived, int _Dim, int _Mode, int _Options>
+void
+vectorToTransform (Eigen::Transform<typename Derived::RealScalar,
+				    _Dim, _Mode, _Options>& transform,
+		   const Eigen::MatrixBase<Derived>& result);
+
+# include <roboptim/retargeting/eigen-rigid-body.hxx>
+#endif //! ROBOPTIM_RETARGETING_EIGEN_RIGID_BODY_HH
---
+++ include/roboptim/retargeting/eigen-rigid-body.hxx
@@ -0,0 +1,136 @@
+#ifndef ROBOPTIM_RETARGETING_EIGEN_RIGID_BODY_HXX
+# define ROBOPTIM_RETARGETING_EIGEN_RIGID_BODY_HXX
+# include <cmath>
+
+template <typename Derived, typename OtherDerived>
+void
+eulerToTransform (Eigen::MatrixBase<Derived> const& linear,
+		  const Eigen::MatrixBase<OtherDerived>& eulerAngles)
+{
+  EIGEN_STATIC_ASSERT_MATRIX_SPECIFIC_SIZE (Derived, 3, 3);
+  EIGEN_STATIC_ASSERT_VECTOR_SPECIFIC_SIZE (OtherDerived, 3);
+
+  // cr means cos roll, sy sin yaw, etc.
+  // sincos computes sinus and cosinus in parallel
+  double cr, sr, cp, sp, cy, sy;
+  sincos (eulerAngles (0), &sr, &cr);
+  sincos (eulerAngles (1), &sp, &cp);
+  sincos (eulerAngles (2), &sy, &cy);
+
+  Eigen::MatrixBase<Derived>& linear_ =
+    const_cast<Eigen::MatrixBase<Derived>&> (linear);
+
+  linear_ (0, 0) = cy * cp;
+  linear_ (0, 1) = cy * sp * sr - sy * cr;
+  linear_ (0, 2) = cy * sp * cr + sy * sr;
+  linear_ (1, 0) = sy * cp;
+  linear_ (1, 1) = sy * sp * sr + cy * cr;
+  linear_ (1, 2) = sy * sp * cr - cy * sr;
+  linear_ (2, 0) = -sp;
+  linear_ (2, 1) = cp * sr;
+  linear_ (2, 2) = cp * cr;
+}
+
+template <typename Derived, typename OtherDerived>
+void
+transformToEuler (Eigen::MatrixBase<Derived> const& eulerAngles,
+		  const Eigen::MatrixBase<OtherDerived>& linear)
+{
+  EIGEN_STATIC_ASSERT_VECTOR_SPECIFIC_SIZE (Derived, 3);
+  EIGEN_STATIC_ASSERT_MATRIX_SPECIFIC_SIZE (OtherDerived, 3, 3);
+
+  Eigen::MatrixBase<Derived>& eulerAngles_ =
+    const_cast<Eigen::MatrixBase<Derived>&> (eulerAngles);
+
+  const double nx = linear (2, 2);
+  const double ny = linear (2, 1);
+
+  eulerAngles_[0] = std::atan2 (ny, nx);
+  eulerAngles_[1] = std::atan2 (-linear (2, 0),
+			       std::sqrt (ny * ny + nx * nx));
+  eulerAngles_[2] = std::atan2 (linear (1, 0), linear (0, 0));
+}
+
+template <typename Derived, typename OtherDerived>
+void
+hat (Eigen::MatrixBase<Derived> const& result,
+     const Eigen::MatrixBase<OtherDerived>& p)
+{
+  EIGEN_STATIC_ASSERT_MATRIX_SPECIFIC_SIZE (Derived, 3, 3);
+  EIGEN_STATIC_ASSERT_VECTOR_SPECIFIC_SIZE (OtherDerived, 3);
+
+  Eigen::MatrixBase<Derived>& result_ =
+    const_cast<Eigen::MatrixBase<Derived>&> (result);
+
+  result_ (0, 0) = 0.;
+  result_ (0, 1) = -p[2];
+  result_ (0, 2) = p[1];
+
+  result_ (1, 0) = p[2];
+  result_ (1, 1) = 0.;
+  result_ (1, 2) = -p[0];
+
+  result_ (2, 0) = -p[1];
+  result_ (2, 1) = p[0];
+  result_ (2, 2) = 0.;
+}
+
+template <typename Derived>
+Eigen::Matrix<typename Derived::RealScalar, 3, 3>
+hat (const Eigen::MatrixBase<Derived>& p)
+{
+  Eigen::Matrix<typename Derived::RealScalar, 3, 3> result;
+  hat (result, p);
+  return result;
+}
+
+template <typename Derived, int _Dim, int _Mode, int _Options>
+void
+adjoint (Eigen::MatrixBase<Derived> const& adjoint,
+	 const Eigen::Transform<typename Derived::RealScalar,
+				_Dim, _Mode, _Options>& transform)
+{
+  EIGEN_STATIC_ASSERT_MATRIX_SPECIFIC_SIZE (Derived, 6, 6);
+
+  Eigen::MatrixBase<Derived>& adjoint_ =
+    const_cast<Eigen::MatrixBase<Derived>&> (adjoint);
+
+  adjoint_.template block<3, 3> (0, 0) = transform.linear ();
+
+  hat (adjoint_.template block<3, 3> (0, 3), transform.translation ());
+  adjoint_.template block<3, 3> (0, 3) *= transform.linear ();
+
+  adjoint_.template block<3, 3> (3, 0).setZero ();
+  adjoint_.template block<3, 3> (3, 3) = transform.linear ();
+}
+
+template <typename Derived, int _Dim, int _Mode, int _Options>
+void
+transformToVector (Eigen::MatrixBase<Derived> const& result,
+		   const Eigen::Transform<typename Derived::RealScalar,
+					  _Dim, _Mode, _Options>& transform)
+{
+  //EIGEN_STATIC_ASSERT_VECTOR_ONLY (Derived);
+  assert (result.size () == 6);
+
+  Eigen::MatrixBase<Derived>& result_ =
+    const_cast<Eigen::MatrixBase<Derived>&> (result);
+
+  result_.template segment<3> (0) = transform.translation ();
+  transformToEuler (result_.template segment<3> (3), transform.linear ());
+}
+
+template <typename Derived, int _Dim, int _Mode, int _Options>
+void
+vectorToTransform (Eigen::Transform<typename Derived::RealScalar,
+				    _Dim, _Mode, _Options>& transform,
+		   const Eigen::MatrixBase<Derived>& result)
+{
+  EIGEN_STATIC_ASSERT_VECTOR_ONLY (Derived);
+  assert (result.size () == 6);
+
+  transform.translation () = result.template segment<3> (0);
+  eulerToTransform (transform.linear (), result.template segment<3> (3));
+}
+
+#endif //! ROBOPTIM_RETARGETING_EIGEN_RIGID_BODY_HXX
